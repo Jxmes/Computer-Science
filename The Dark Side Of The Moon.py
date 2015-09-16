7b33f2a99c6881fedcba28fec90f8f31b0cc0861
Jxmes <imslloths@gmail.com>
2015-09-16 12:55:08 +0100

    Create The Dark Side Of The Moon.py

---
+++ The Dark Side Of The Moon.py
@@ -0,0 +1,26 @@
+import time
+
+time.sleep(1)
+
+
+
+print("                                                               ")
+print("                                                               ")
+print("                                                               ")
+print("                                                               ")
+print("                                                               ")
+print("                                                               ")
+print("                                                               ")
+print("                           THE DARK SIDE OF THE MOON")
+
+time.sleep(1)
+
+print("                                                               ")
+print("                                     /\    ___--- ")
+print("                              ------/  \---       ")
+print("                              -----/    \--------- ")
+print("                             -----/      \---___ ")
+print("                                 /        \     --- ")
+print("                                /__________\ ")
+
+time.sleep(10)
